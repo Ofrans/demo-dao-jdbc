ac587b04329f56fe5969a596348d174717c20c1e
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-08-23 16:08:39 -0300

    SellerDaoJDBC and DaoFactory

---
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.dao.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller Seller) {
+
+    }
+
+    @Override
+    public void update(Seller Seller) {
+
+    }
+
+    @Override
+    public void deleteById(Seller Seller) {
+
+    }
+
+    @Override
+    public Seller getById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return List.of();
+    }
+}
