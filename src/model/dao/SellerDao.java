c70c7ea4fc01aedc2dae04060f4dd0bc8fa4ba78
Fransergio Filho <fransergio.morato.pb@compasso.com.br>
2024-08-23 16:02:00 -0300

    Seller Dao e Department Dao interfaces

---
+++ src/model/dao/SellerDao.java
@@ -0,0 +1,13 @@
+package model.dao;
+
+import model.entities.Seller;
+
+import java.util.List;
+
+public interface SellerDao {
+    void insert(Seller Seller);
+    void update(Seller Seller);
+    void deleteById(Seller Seller);
+    Seller getById(Integer id);
+    List<Seller> findAll();
+}
